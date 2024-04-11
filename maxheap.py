5bd14881c2c25f932a9022c8f6cde92c403308dc
Charlie Doherty <charlieedoherty@gmail.com>
2024-04-10 19:58:52 -0500

    Create the remove and _downheap methods

--- maxheap.py
+++ maxheap.py
@@ -2,6 +2,11 @@ class MaxHeap:
     def __init__(self):
         self._heap = []
 
+    def length(self):
+        """Gets a count of how many elements are in the heap"""
+
+        return len(self._heap)
+
     def add(self, entry):
         """Adds an element to the maxheap"""
 
@@ -25,19 +30,50 @@ class MaxHeap:
                 temp = self._heap[parent_i]
                 self._heap[parent_i] = self._heap[index]
                 self._heap[index] = temp
-                return self._upheap(parent_i)
+                self._upheap(parent_i)
 
     def remove(self):
-        """Replaces the max element with the 'lowest, right-most' element and upheaps"""
+        """Removes the first index, or top/max element"""
 
-        pass
+        # If the heap is empty, raise an Error
+        if self.length() == 0:
+            raise RuntimeError
 
-    def length(self):
-        """Gets a count of how many elements are in the heap"""
+        # If there's only one element in the heap, remove it
+        elif self.length() == 1:
+            self._heap = []
 
-        return len(self._heap)
+        # When there's more than one element, replace it with the lowest, right-most element and remove it
+        else:
+            self._heap[0] = self._heap.pop(-1)
+
+            # Upheap the new value to the parent element
+            self._downheap(0)
 
     def _downheap(self, index):
         """Recursively moves the element at the given index down"""
 
-        pass
+        l_child_i = 2*index+1
+        r_child_i = 2*index+2
+
+        # If there is no left child then do nothing
+        if l_child_i >= self.length():
+            pass
+
+        # If the left child is bigger than the current element then switch them
+        elif self._heap[l_child_i] > self._heap[index]:
+            temp = self._heap[l_child_i]
+            self._heap[l_child_i] = self._heap[index]
+            self._heap[index] = temp
+            self._downheap(l_child_i)
+
+        # If there is no right child then do nothing
+        if r_child_i >= self.length():
+            pass
+
+        # If the left child is bigger than the current element (after checking the right) then switch them
+        elif self._heap[r_child_i] > self._heap[index]:
+            temp = self._heap[r_child_i]
+            self._heap[r_child_i] = self._heap[index]
+            self._heap[index] = temp
+            self._downheap(r_child_i)
